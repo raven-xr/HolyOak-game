4606456b561f2cf4b5c0b6c037eb2589b01f95a8
Raven <raven-esq@yandex.ru>
2024-07-11 10:57:53 +0700

    Improved main menu
    * Add tutorial button
    * Updated play button: now it opens list of levels instead of tutorial
    * Now quit and sets buttons work momentally
    * Some small feautures

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,28 +1,44 @@
 extends Node2D
 @onready var click_2d = $SFX/Click2D
 @onready var radio = $SFX/Radio
+@onready var play_texture_button = $UserInterface/VBoxContainer/Play_TextureButton
+@onready var tutorial_texture_button = $UserInterface/VBoxContainer/Tutorial_TextureButton
+@onready var settings_texture_button = $UserInterface/VBoxContainer/Settings_TextureButton
+@onready var quit_texture_button = $UserInterface/VBoxContainer/Quit_TextureButton
 
 func _ready():
 	var tween = get_tree().create_tween()
-	tween.tween_property(radio, "volume_db", -5.0, 5.0)
+	tween.tween_property(radio, "volume_db", -4.0, 4.0)
 
 func _on_play_texture_button_pressed():
-	change_scene()
-	await get_tree().create_timer(8.0).timeout
+	click_2d.play()
+	change_level()
+	await get_tree().create_timer(6.0).timeout
+	get_tree().change_scene_to_file("res://scenes/levels/levels.tscn")
+
+func _on_tutorial_texture_button_pressed():
+	click_2d.play()
+	change_level()
+	await get_tree().create_timer(6.0).timeout
 	get_tree().change_scene_to_file("res://scenes/tutorial/tutorial.tscn")
 
 func _on_settings_texture_button_pressed():
-	change_scene()
-	await get_tree().create_timer(8.0).timeout
+	click_2d.play()
+	await click_2d.finished
 	get_tree().change_scene_to_file("res://scenes/settings/settings.tscn")
 
 func _on_quit_texture_button_pressed():
-	change_scene()
-	await get_tree().create_timer(8.0).timeout
+	click_2d.play()
+	await click_2d.finished
 	get_tree().quit()
 
-func change_scene():
+func change_level():
+	play_texture_button.disabled = true
+	tutorial_texture_button.disabled = true
+	settings_texture_button.disabled = true
+	quit_texture_button.disabled = true
+	
 	var tween_1 = get_tree().create_tween()
-	tween_1.parallel().tween_property(self, "modulate", Color(0, 0, 0, 1), 4.0)
+	tween_1.parallel().tween_property(self, "modulate", Color(0, 0, 0, 1), 3.0)
 	var tween_2 = get_tree().create_tween()
-	tween_2.parallel().tween_property(radio, "volume_db", -100, 8.0)
+	tween_2.parallel().tween_property(radio, "volume_db", -100, 6.0)
