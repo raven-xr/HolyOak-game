11e23adf2e0acaf0d254792a371e5958671be98c
Raven <raven-esq@yandex.ru>
2024-07-11 13:26:58 +0700

    Add radio and smooth transition
    * Add smooth transition to the tutorial level
    * Add songs A. N. - Traveler for idle and A. N. - Leaving for Valhalla for fight

--- scenes/tutorial/tutorial.gd
+++ scenes/tutorial/tutorial.gd
@@ -1,1 +1,19 @@
 extends Node2D
+@onready var radio_idle = $SFX/RadioIdle
+@onready var radio_fight = $SFX/RadioFight
+
+func _ready():
+	self.modulate = Color(0, 0, 0, 1)
+	
+	radio_idle.play()
+	var tween_1 = get_tree().create_tween()
+	tween_1.parallel().tween_property(self, "modulate", Color(1, 1, 1, 1), 2.0)
+	var tween_2 = get_tree().create_tween()
+	tween_2.parallel().tween_property(radio_idle, "volume_db", -12, 4.0)
+
+func fight():
+	radio_fight.play()
+	var tween_3 = get_tree().create_tween()
+	tween_3.parallel().tween_property(radio_idle, "volume_db", -100, 4.0)
+	var tween_4 = get_tree().create_tween()
+	tween_4.parallel().tween_property(radio_fight, "volume_db", -12, 4.0)
