b7ff6668c405e296fb880b1aea0d78a808d884d3
Raven <raven-esq@yandex.ru>
2024-08-30 12:01:44 +0700

    Update theme structure
    * Add main menu to panel container
    * Add message.tscn to panel container

--- subscenes/message/message.gd
+++ subscenes/message/message.gd
@@ -2,17 +2,18 @@ extends CanvasLayer
 
 var text: String # The value is given by the parent
 
-@onready var label = $Label
+@onready var panel_container = $PanelContainer
+@onready var label = $PanelContainer/Label
 
 func _ready():
-	label.modulate = Color(1, 1, 1, 0)
+	panel_container.modulate = Color(1, 1, 1, 0)
 	label.text = text
-	label.size.x = len(text) * 10.0
 	
 	var tween_1 = create_tween()
-	tween_1.tween_property(label, "modulate", Color(1, 1, 1, 1), 0.5)
+	tween_1.tween_property(panel_container, "modulate", Color(1, 1, 1, 1), 1.5)
 	await tween_1.finished
+	await get_tree().create_timer(1.5).timeout
 	var tween_2 = create_tween()
-	tween_2.tween_property(label, "modulate", Color(1, 1, 1, 0), 3.0)
+	tween_2.tween_property(panel_container, "modulate", Color(1, 1, 1, 0), 1.5)
 	await tween_2.finished
 	queue_free()
