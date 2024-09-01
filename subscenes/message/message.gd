86d65e8f56bc90278e2d01fb1c0e9c530b211e12
Raven <raven-esq@yandex.ru>
2024-09-01 11:48:33 +0700

    Update message.tscn
    * Make message a little more transparent

--- subscenes/message/message.gd
+++ subscenes/message/message.gd
@@ -10,7 +10,7 @@ func _ready():
 	label.text = text
 	
 	var tween_1 = create_tween()
-	tween_1.tween_property(panel_container, "modulate", Color(1, 1, 1, 1), 1.5)
+	tween_1.tween_property(panel_container, "modulate", Color(1, 1, 1, 0.78125), 1.5) # 0.784314 = A: 200
 	await tween_1.finished
 	await get_tree().create_timer(1.5).timeout
 	var tween_2 = create_tween()
