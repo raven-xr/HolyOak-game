11ac9de73d215310389429fcf4c9077bf09b876f
Raven <raven-esq@yandex.ru>
2024-09-05 10:22:27 +0700

    Add HP value to the ui stats

--- subscenes/ui_stats/ui_stats.gd
+++ subscenes/ui_stats/ui_stats.gd
@@ -2,13 +2,18 @@ extends CanvasLayer
 
 @onready var money_value = $PanelContainer/HBoxContainer/MoneyValue
 @onready var wave_value = $PanelContainer/HBoxContainer/WaveValue
+@onready var hp_value = $PanelContainer/HBoxContainer/HPValue
 
 func _ready():
 	Signals.connect("money_change", Callable(self, "_on_money_change"))
 	Signals.connect("wave_change", Callable(self, "_on_wave_change"))
+	Signals.connect("health_change", Callable(self, "_on_health_change"))
 
 func _on_money_change(value):
 	money_value.text = str(value)
 	
 func _on_wave_change(value):
 	wave_value.text = str(value)
+
+func _on_health_change(value):
+	hp_value.text = str(value)
