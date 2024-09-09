b67fa87ec88e8136eaeda5b9a36056cf454c3dbd
Raven <raven-esq@yandex.ru>
2024-09-09 19:33:20 +0700

    Fixed a bug when leaves fell off at the beginning of the game

--- subscenes/holy_oak/holy_oak.gd
+++ subscenes/holy_oak/holy_oak.gd
@@ -7,5 +7,6 @@ func _ready():
 	Signals.connect("health_change", Callable(self, "_on_health_change"))
 
 func _on_health_change(health):
-	leaves.emitting = true
+	if health_bar.value > health:
+		leaves.emitting = true
 	health_bar.value = health
