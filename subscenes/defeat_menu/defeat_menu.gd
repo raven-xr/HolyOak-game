481c7ab4aa22b90eb413383552a9cb07f5cb1854
Raven <raven-esq@yandex.ru>
2024-09-14 09:45:03 +0700

    Make "NextButton" take the path level from the var
    * Make "Next Button" take the path to the next level from the current level variable

--- subscenes/defeat_menu/defeat_menu.gd
+++ subscenes/defeat_menu/defeat_menu.gd
@@ -17,4 +17,4 @@ func _on_menu_button_pressed():
 	click.play()
 	await click.finished
 	get_tree().paused = false
-	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
+	get_tree().change_scene_to_file(level.next_level_path)
