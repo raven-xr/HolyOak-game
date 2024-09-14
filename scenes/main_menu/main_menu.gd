b217992196ca167298bd5b4ba3237bca9cb39433
Raven <raven-esq@yandex.ru>
2024-09-14 09:56:18 +0700

    Update main_menu.gd
    * Remove some debugging code

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -10,10 +10,6 @@ extends Node2D
 func _ready():
 	var tween = get_tree().create_tween()
 	tween.tween_property(radio, "volume_db", -4.0, 4.0)
-	#await tween.finished # DEBUG
-	#change_level() # DEBUG
-	#await get_tree().create_timer(4.0).timeout # DEBUG
-	#get_tree().call_deferred("change_scene_to_file", "res://debug/test_scene.tscn") # DEBUG
 
 func _on_play_texture_button_pressed():
 	click_2d.play()
@@ -42,9 +38,7 @@ func change_level():
 	tutorial_texture_button.disabled = true
 	settings_texture_button.disabled = true
 	quit_texture_button.disabled = true
-	
 	var tween_1 = get_tree().create_tween()
 	tween_1.parallel().tween_property(self, "modulate", Color(0, 0, 0, 1), 2.0)
-	
 	var tween_2 = get_tree().create_tween()
 	tween_2.parallel().tween_property(radio, "volume_db", -100, 4.0)
