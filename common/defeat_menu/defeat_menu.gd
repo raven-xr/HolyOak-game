5b48e507c04973fccb45bab2cbf8f6344693e62f
Raven <raven-esq@yandex.ru>
2024-09-20 19:50:57 +0700

    Remove commented code

--- common/defeat_menu/defeat_menu.gd
+++ common/defeat_menu/defeat_menu.gd
@@ -12,7 +12,6 @@ func _on_restart_button_pressed():
 	SoundManager.click.play()
 	SoundManager.disable_music()
 	change_level()
-	#await get_tree().create_timer(4.0).timeout
 	get_tree().paused = false
 	get_tree().reload_current_scene()
 
@@ -20,14 +19,9 @@ func _on_menu_button_pressed():
 	SoundManager.click.play()
 	SoundManager.disable_music()
 	change_level()
-	#await get_tree().create_timer(4.0).timeout
 	get_tree().paused = false
 	get_tree().change_scene_to_file(level.next_level_path)
 
 func change_level():
 	menu_button.disabled = true
 	restart_button.disabled = true
-	#var tween_1 = get_tree().create_tween()
-	#tween_1.parallel().tween_property(level, "modulate", Color(0, 0, 0, 1), 2.0)
-	#var tween_2 = get_tree().create_tween()
-	#tween_2.parallel().tween_property(level.radio_fight, "volume_db", -100, 4.0)
--- common/victory_menu/victory_menu.gd
+++ common/victory_menu/victory_menu.gd
@@ -19,14 +19,9 @@ func _on_menu_button_pressed():
 	SoundManager.click.play()
 	SoundManager.disable_music()
 	change_level()
-	#await get_tree().create_timer(4.0).timeout
 	get_tree().paused = false
 	get_tree().change_scene_to_file("res://scenes/main_menu/main_menu.tscn")
 
 func change_level():
 	menu_button.disabled = true
 	next_button.disabled = true
-	#var tween_1 = create_tween()
-	#tween_1.parallel().tween_property(level, "modulate", Color(0, 0, 0, 1), 2.0)
-	#var tween_2 = create_tween()
-	#tween_2.parallel().tween_property(level.radio_fight, "volume_db", -100, 4.0)
