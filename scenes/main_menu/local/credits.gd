50a12871f9bbfa0ad23e0bd38146252a7a10d236
Raven <raven-esq@yandex.ru>
2024-10-03 09:06:12 +0700

    Remove needless "Control" node

--- scenes/main_menu/local/credits.gd
+++ scenes/main_menu/local/credits.gd
@@ -1,15 +1,15 @@
 extends CanvasLayer
 
-@onready var control = $Control
+@onready var panel_container = $PanelContainer
 
 func _ready():
-	control.modulate = Color(1, 1, 1, 0)
+	panel_container.modulate = Color(1, 1, 1, 0)
 	var tween = get_tree().create_tween()
-	tween.tween_property(control, "modulate", Color(1, 1, 1, 1), 0.1)
+	tween.tween_property(panel_container, "modulate", Color(1, 1, 1, 1), 0.1)
 
 func _on_close_button_pressed():
 	SoundManager.click.play()
 	var tween = get_tree().create_tween()
-	tween.tween_property(control, "modulate", Color(1, 1, 1, 0), 0.1)
+	tween.tween_property(panel_container, "modulate", Color(1, 1, 1, 0), 0.1)
 	await tween.finished
 	queue_free()
