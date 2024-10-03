68b07e154cd4d140ba19194f668192f8024fc7c2
Raven <raven-esq@yandex.ru>
2024-10-03 08:25:52 +0700

    Remove "Texture" prefixes from names

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,42 +1,42 @@
 extends Node2D
 
-@onready var play_texture_button = $"UserInterface/VBoxContainer/Play TextureButton"
-@onready var tutorial_texture_button = $"UserInterface/VBoxContainer/Tutorial TextureButton"
-@onready var settings_texture_button = $"UserInterface/VBoxContainer/Settings TextureButton"
-@onready var quit_texture_button = $"UserInterface/VBoxContainer/Quit TextureButton"
+@onready var play_button = $"UserInterface/VBoxContainer/Play Button"
+@onready var tutorial_button = $"UserInterface/VBoxContainer/Tutorial Button"
+@onready var settings_button = $"UserInterface/VBoxContainer/Settings Button"
+@onready var quit_button = $"UserInterface/VBoxContainer/Quit Button"
 
 func _ready():
 	SoundManager.music_main.play()
 	SoundManager.music_main.volume_db = -20
 
-func _on_play_texture_button_pressed():
+func _on_play_button_pressed():
 	SoundManager.click.play()
 	change_level()
 	await get_tree().create_timer(4.0).timeout
 	SoundManager.disable_music()
 	get_tree().change_scene_to_file("res://scenes/levels/levels.tscn")
 
-func _on_tutorial_texture_button_pressed():
+func _on_tutorial_button_pressed():
 	SoundManager.click.play()
 	change_level()
 	await get_tree().create_timer(4.0).timeout
 	SoundManager.disable_music()
 	get_tree().change_scene_to_file("res://scenes/tutorial/tutorial.tscn")
 
-func _on_settings_texture_button_pressed():
+func _on_settings_button_pressed():
 	SoundManager.click.play()
 	get_tree().change_scene_to_file("res://scenes/settings/settings.tscn")
 
-func _on_quit_texture_button_pressed():
+func _on_quit_button_pressed():
 	SoundManager.click.play()
 	await SoundManager.click.finished
 	get_tree().quit()
 
 func change_level():
-	play_texture_button.disabled = true
-	tutorial_texture_button.disabled = true
-	settings_texture_button.disabled = true
-	quit_texture_button.disabled = true
+	play_button.disabled = true
+	tutorial_button.disabled = true
+	settings_button.disabled = true
+	quit_button.disabled = true
 	var tween_1 = get_tree().create_tween()
 	tween_1.parallel().tween_property(self, "modulate", Color(0, 0, 0, 1), 2.0)
 	var tween_2 = get_tree().create_tween()
