22ae1f5a788f2464ac561a90a4616fc9e7bb369c
Raven <raven-esq@yandex.ru>
2024-10-05 11:59:39 +0700

    Improve the code and the animation of transition
    * Rename "UserInterface" to "Explorer"
    * Decrease the duration of the 1st and the 2nd tweens in the "change_level" func

--- scenes/main_menu/main_menu.gd
+++ scenes/main_menu/main_menu.gd
@@ -1,9 +1,9 @@
 extends Node2D
 
-@onready var play_button = $"UserInterface/VBoxContainer/Play Button"
-@onready var tutorial_button = $"UserInterface/VBoxContainer/Tutorial Button"
-@onready var settings_button = $"UserInterface/VBoxContainer/Settings Button"
-@onready var quit_button = $"UserInterface/VBoxContainer/Quit Button"
+@onready var play_button = $"Explorer/VBoxContainer/Play Button"
+@onready var tutorial_button = $"Explorer/VBoxContainer/Tutorial Button"
+@onready var settings_button = $"Explorer/VBoxContainer/Settings Button"
+@onready var quit_button = $"Explorer/VBoxContainer/Quit Button"
 
 @export var credits_scene: PackedScene
 
@@ -20,7 +20,7 @@ func _on_play_button_pressed():
 func _on_tutorial_button_pressed():
 	SoundManager.click.play()
 	change_level()
-	await get_tree().create_timer(4.0).timeout
+	await SoundManager.click.finished
 	SoundManager.disable_music()
 	get_tree().change_scene_to_file("res://scenes/tutorial/tutorial.tscn")
 
@@ -34,17 +34,14 @@ func _on_settings_button_pressed():
 
 func _on_quit_button_pressed():
 	SoundManager.click.play()
-	var tween = create_tween()
-	tween.tween_property(self, "modulate", Color(0, 0, 0, 1), 0.1)
+	change_level()
 	await SoundManager.click.finished
 	get_tree().quit()
 
 func change_level():
-	play_button.disabled = true
-	tutorial_button.disabled = true
-	settings_button.disabled = true
-	quit_button.disabled = true
+	for button in $Explorer/VBoxContainer.get_children():
+		button.disabled = true
 	var tween_1 = get_tree().create_tween()
-	tween_1.parallel().tween_property(self, "modulate", Color(0, 0, 0, 1), 2.0)
+	tween_1.parallel().tween_property(self, "modulate", Color(0, 0, 0, 1), 0.1)
 	var tween_2 = get_tree().create_tween()
-	tween_2.parallel().tween_property(SoundManager.music_main, "volume_db", -100, 4.0)
+	tween_2.parallel().tween_property(SoundManager.music_main, "volume_db", -100, 0.2)
