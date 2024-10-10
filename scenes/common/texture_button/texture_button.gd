96af54c9c175ba628b3fc21e0549de9a8a7acf0d
Raven <raven-esq@yandex.ru>
2024-10-10 18:04:38 +0700

    Update texture_button.gd
    * Remove a needless variable

--- scenes/common/texture_button/texture_button.gd
+++ scenes/common/texture_button/texture_button.gd
@@ -2,13 +2,10 @@ extends TextureButton
 
 @onready var label = $Label
 
-var is_label_hovered: bool = false
-
 func _on_mouse_entered():
 	if not disabled:
 		label.position.y -= 1
-		is_label_hovered = true
 
 func _on_mouse_exited():
-	if is_label_hovered:
+	if not disabled:
 		label.position.y += 1
