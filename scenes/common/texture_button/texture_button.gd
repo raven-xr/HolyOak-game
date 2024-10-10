31971ed81c79921ca4cc800380b3c1743d0ea6c0
Raven <raven-esq@yandex.ru>
2024-10-10 17:16:45 +0700

    Make TextureButton scene label's attributes native
    * Make Play, Credits, Settings, Exit buttons nodes local

--- scenes/common/texture_button/texture_button.gd
+++ scenes/common/texture_button/texture_button.gd
@@ -1,15 +1,8 @@
 extends TextureButton
 
-@export var text: String = 'TEXT'
-@export var font_size: int = 16
-
 @onready var label = $Label
 
 var is_label_hovered: bool = false
-
-func _ready():
-	label.set("text", text)
-	label.set("theme_override_font_sizes/font_size", font_size)
 
 func _on_mouse_entered():
 	if not disabled:
