0719d9af83cfcd7d1721f728dcd1b4fb3dea3897
Raven <raven-esq@yandex.ru>
2024-10-29 12:19:13 +0700

    Fix a bug, decrease volume
    * Decrease volume
    * Fix a bug: when the player turned off and turned on the menu, the slider value was reset
    * Fix uneven volume distribution on the slider

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -3,10 +3,22 @@ extends CanvasLayer
 # Nodes
 @onready var control = $Control
 
+@onready var master_h_slider = $"Control/PanelContainer/VBoxContainer/Master Volume/Master HSlider"
+@onready var music_h_slider = $"Control/PanelContainer/VBoxContainer/Music Volume/Music HSlider"
+@onready var sfx_h_slider = $"Control/PanelContainer/VBoxContainer/SFX Volume/SFX HSlider"
+
+# Buses
+var master_bus = AudioServer.get_bus_index("Master")
+var music_bus = AudioServer.get_bus_index("Music")
+var sfx_bus = AudioServer.get_bus_index("SFX")
+
 
 
 # Common functions
 func _ready():
+	master_h_slider.value = db_to_linear(AudioServer.get_bus_volume_db(master_bus))
+	music_h_slider.value = db_to_linear(AudioServer.get_bus_volume_db(music_bus))
+	sfx_h_slider.value = db_to_linear(AudioServer.get_bus_volume_db(sfx_bus))
 	control.modulate = Color(1, 1, 1, 0)
 
 func _on_visibility_changed():
@@ -18,17 +30,14 @@ func _on_visibility_changed():
 
 
 # Volume's functions
-func _on_master_bus_value_changed(value):
-	var master_bus = AudioServer.get_bus_index("Master")
-	AudioServer.set_bus_volume_db(master_bus, value)
+func _on_master_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(master_bus, linear_to_db(value))
 
-func _on_music_bus_value_changed(value):
-	var music_bus = AudioServer.get_bus_index("Music")
-	AudioServer.set_bus_volume_db(music_bus, value)
+func _on_music_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(music_bus, linear_to_db(value))
 
-func _on_sfx_bus_value_changed(value):
-	var sfx_bus = AudioServer.get_bus_index("SFX")
-	AudioServer.set_bus_volume_db(sfx_bus, value)
+func _on_sfx_h_slider_value_changed(value):
+	AudioServer.set_bus_volume_db(sfx_bus, linear_to_db(value))
 
 
 
