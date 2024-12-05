f5666a2e0b0f5915308ddef7350de53f603ef08e
Raven <raven-esq@yandex.ru>
2024-12-05 11:46:14 +0700

    Add an ability to split

--- enemies/slime/slime.gd
+++ enemies/slime/slime.gd
@@ -2,9 +2,33 @@ extends Enemy
 
 
 
-# Called by AnimationPlayer
+# Common
+func split():
+	animation_player.play(direction + "_Split")
+	await animation_player.animation_finished
+	
+	position -= Vector2(4.0, 4.0)
+	
+	var new_path_follow_2d = path_follow_2d.duplicate()
+	new_path_follow_2d.get_child(0).position += Vector2(4.0, 4.0)
+	
+	path_follow_2d.add_sibling(new_path_follow_2d)
+	
+	match state:
+		States.MOVE: move_state()
+		States.ATTACK: attack_state()
+		States.DEATH: death_state()
+
+
+
+# AnimationPlayer
 func rest(): # Cooldown between jumps
 	path_follow_2d.speed = 0.0
 
 func jump():
 	path_follow_2d.speed = stats["speed"]
+
+
+# Timer
+func _on_timer_timeout():
+	split()
