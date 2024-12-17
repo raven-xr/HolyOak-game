523de9e1b6ee014c0bd539702b7b6d72e5705bcc
Raven <raven-esq@yandex.ru>
2024-12-17 16:12:39 +0700

    Rebalance UnitData

--- autoload/data/UnitData.gd
+++ autoload/data/UnitData.gd
@@ -17,36 +17,36 @@ const ARCHER = {
 		"attack_range": 225
 	},
 	"level_3": {
-		"damage": 30,
-		"cost": 600,
+		"damage": 20,
+		"cost": 800,
 		"unit_count": 2,
 		"spawnpoints": [Vector2(-14, -35), Vector2(16, -35)],
 		"attack_range": 250
 	},
 	"level_4": {
-		"damage": 40,
-		"cost": 800,
+		"damage": 30,
+		"cost": 1200,
 		"unit_count": 2,
 		"spawnpoints": [Vector2(16, -32), Vector2(-16, -32)],
 		"attack_range": 275
 	},
 	"level_5": {
-		"damage": 50,
-		"cost": 1500,
+		"damage": 30,
+		"cost": 2700,
 		"unit_count": 3,
 		"spawnpoints": [Vector2(0, -42), Vector2(-16, -32), Vector2(16, -32)],
 		"attack_range": 300
 	},
 	"level_6": {
-		"damage": 60,
-		"cost": 1800,
+		"damage": 40,
+		"cost": 3600,
 		"unit_count": 3,
 		"spawnpoints": [Vector2(0, -44), Vector2(-16, -34), Vector2(16, -34)],
 		"attack_range": 325
 	},
 	"level_7": {
-		"damage": 70,
-		"cost": 2100,
+		"damage": 50,
+		"cost": 4500,
 		"unit_count": 3,
 		"spawnpoints": [Vector2(0, -44), Vector2(-16, -34), Vector2(16, -34)],
 		"attack_range": 350
