6692f19ca432cb302c0bd589f2581a4fbad21e20
Raven <raven-esq@yandex.ru>
2025-03-01 08:47:19 +0700

    Update EffectData.gd

--- gamedata/autoload/data/EffectData.gd
+++ gamedata/autoload/data/EffectData.gd
@@ -1,6 +1,11 @@
 extends Node
 
+#const EXAMPLE = {
+	#"damage": 1, # per second (int)
+	#"duration": 5 # seconds (int)
+#}
+
 const ARSON = {
-	"damage": 3, # per second (int)
-	"duration": 5 # seconds (int)
+	"damage": 3,
+	"duration": 5
 }
