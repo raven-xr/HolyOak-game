786f8b7b51d693bd80866d080fa64077fbacc59a
Raven <raven-esq@yandex.ru>
2025-03-08 13:54:35 +0700

    Update costs (fire archer)

--- gamedata/autoload/data/UnitData.gd
+++ gamedata/autoload/data/UnitData.gd
@@ -74,7 +74,7 @@ const FIRE_ARCHER = {
 	"level_1": {
 		"shell_speed": 7000,
 		"damage": 10,
-		"cost": 100,
+		"cost": 150,
 		"unit_count": 1,
 		"spawnpoints": [Vector2(0, 0)],
 		"attack_range": 200
@@ -82,7 +82,7 @@ const FIRE_ARCHER = {
 	"level_2": {
 		"shell_speed": 7000,
 		"damage": 20,
-		"cost": 300,
+		"cost": 450,
 		"unit_count": 1,
 		"spawnpoints": [Vector2(0, -20)],
 		"attack_range": 225
@@ -90,7 +90,7 @@ const FIRE_ARCHER = {
 	"level_3": {
 		"shell_speed": 7000,
 		"damage": 20,
-		"cost": 1000,
+		"cost": 1500,
 		"unit_count": 2,
 		"spawnpoints": [Vector2(-14, -35), Vector2(16, -35)],
 		"attack_range": 250
@@ -98,7 +98,7 @@ const FIRE_ARCHER = {
 	"level_4": {
 		"shell_speed": 7000,
 		"damage": 30,
-		"cost": 1800,
+		"cost": 2700,
 		"unit_count": 2,
 		"spawnpoints": [Vector2(16, -32), Vector2(-16, -32)],
 		"attack_range": 275
@@ -106,7 +106,7 @@ const FIRE_ARCHER = {
 	"level_5": {
 		"shell_speed": 7000,
 		"damage": 30,
-		"cost": 3600,
+		"cost": 5400,
 		"unit_count": 3,
 		"spawnpoints": [Vector2(0, -42), Vector2(-16, -32), Vector2(16, -32)],
 		"attack_range": 300
@@ -114,7 +114,7 @@ const FIRE_ARCHER = {
 	"level_6": {
 		"shell_speed": 7000,
 		"damage": 40,
-		"cost": 4800,
+		"cost": 7200,
 		"unit_count": 3,
 		"spawnpoints": [Vector2(0, -44), Vector2(-16, -34), Vector2(16, -34)],
 		"attack_range": 325
@@ -122,7 +122,7 @@ const FIRE_ARCHER = {
 	"level_7": {
 		"shell_speed": 7000,
 		"damage": 50,
-		"cost": 9600,
+		"cost": 14400,
 		"unit_count": 3,
 		"spawnpoints": [Vector2(0, -44), Vector2(-16, -34), Vector2(16, -34)],
 		"attack_range": 350
