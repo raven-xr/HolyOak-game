c8c4cc6adc604b8b2a29a942d6d91d9d2373d93e
Raven <raven-esq@yandex.ru>
2025-04-20 09:45:45 +0700

    Update smoke.gd

--- gamedata/FX/smoke/smoke.gd
+++ gamedata/FX/smoke/smoke.gd
@@ -5,20 +5,20 @@ var animations: Array[StringName] = ["Smoke_05_01", "Smoke_05_07", "Smoke_05_08"
 ## And after the last animation is played, the FX is freed
 var is_active: bool = true
 
-func _ready():
+func _ready() -> void:
 	scale.x = randf_range(0.9, 1.1)
 	scale.y = scale.x
 	play(animations[randi_range(0, 2)])
 	shake()
 
-func shake():
+func shake() -> void:
 	var final_value = position - Vector2(randf_range(-2, 2), randf_range(-2, 2))
 	var tween = create_tween()
 	tween.tween_property(self, "position", final_value, 0.2)
 	await tween.finished
 	shake()
 
-func _on_animation_finished():
+func _on_animation_finished() -> void:
 	if is_active:
 		play(animations[randi_range(0, 2)])
 	else:
