22215260037f11392269af5bb845060504eaa66e
Raven <raven-esq@yandex.ru>
2025-04-27 09:21:50 +0700

    Create InterfaceManager.tscn

--- gamedata/autoload/game/SoundManager.gd
+++ gamedata/autoload/game/SoundManager.gd
@@ -1,20 +1,25 @@
 extends Node
 
-@onready var click = $UserInterface/Click
-@onready var success = $UserInterface/Success
-@onready var victory = $UserInterface/Victory
-@onready var defeat = $UserInterface/Defeat
-@onready var music_main = $"Music/Music Main"
-@onready var music_idle = $"Music/Music Idle"
-@onready var music_fight = $"Music/Music Fight"
 
-func disable_music():
+
+@onready var click: AudioStreamPlayer = $"User Interface/Click"
+@onready var success: AudioStreamPlayer = $"User Interface/Success"
+@onready var victory: AudioStreamPlayer = $"User Interface/Victory"
+@onready var defeat: AudioStreamPlayer = $"User Interface/Defeat"
+
+@onready var music_main: AudioStreamPlayer = $"Music/Music Main"
+@onready var music_idle: AudioStreamPlayer = $"Music/Music Idle"
+@onready var music_fight: AudioStreamPlayer = $"Music/Music Fight"
+
+
+
+func disable_music() -> void:
 	music_main.stop()
 	music_idle.stop()
 	music_fight.stop()
 	reset_volume()
 
-func reset_volume():
+func reset_volume() -> void:
 	music_main.volume_db = -20
 	music_idle.volume_db = -40
 	music_fight.volume_db = -40
