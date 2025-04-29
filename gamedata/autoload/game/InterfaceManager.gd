5beb3de5613bd5c168beaf6bc81349d1aefaf91f
Raven <raven-esq@yandex.ru>
2025-04-29 17:30:59 +0700

    Add an ability to open a GUI element

--- gamedata/autoload/game/InterfaceManager.gd
+++ gamedata/autoload/game/InterfaceManager.gd
@@ -1,1 +1,9 @@
 extends Node
+
+## Adds an interface element by its name
+## If there is no such a node, it prints an error
+func add(node_name: StringName = "") -> void:
+	if get(node_name):
+		add_child(get(node_name).instantiate())
+	else:
+		print("ERROR: the \"%s\" node doesn't exist or isn't defined in the interface manager!" % String(node_name))
