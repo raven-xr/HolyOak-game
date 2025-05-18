4e200b070e1aa1df152b32d9880bceed825d94f2
Raven <raven-esq@yandex.ru>
2025-05-18 12:26:47 +0700

    Update GameMenu
    * Remove CanvasLayer
    * Format the code
    * Set up the interaction with the GameController

--- gamedata/ui/game_menu/game_menu.gd
+++ gamedata/ui/game_menu/game_menu.gd
@@ -1,45 +1,43 @@
-extends CanvasLayer
+extends PanelContainer
 
-@onready var panel_container: PanelContainer = $PanelContainer
-@onready var settings: Control = $Settings
-@onready var resume_button: Button = $"PanelContainer/VBoxContainer/Resume Button"
-@onready var settings_button: Button = $"PanelContainer/VBoxContainer/Settings Button"
-@onready var exit_button: Button = $"PanelContainer/VBoxContainer/Exit Button"
+@onready var resume_button: Button = $"VBoxContainer/Resume Button"
+@onready var settings_button: Button = $"VBoxContainer/Settings Button"
+@onready var exit_button: Button = $"VBoxContainer/Exit Button"
 
 func _ready() -> void:
-	panel_container.scale = Vector2(UserSettings.gui_scale**2, UserSettings.gui_scale**2)
-	# Animate
-	panel_container.modulate = Color(1, 1, 1, 0)
+	scale = Vector2(UserSettings.gui_scale, UserSettings.gui_scale)
+	# Smooth appearance
+	modulate = Color(1.0, 1.0, 1.0, 0.0)
 	var tween = create_tween()
-	tween.tween_property(panel_container, "modulate", Color(1, 1, 1, 1), 0.2)
+	tween.tween_property(self, "modulate", Color(1.0, 1.0, 1.0, 1.0), 0.2)
 	await tween.finished
 	# Pause the game
 	get_tree().paused = true
 
-func disable_buttons():
+func disable_buttons() -> void:
 	resume_button.disabled = true
 	settings_button.disabled = true
 	exit_button.disabled = true
 
-func resume(): # Is used by parent/siblings in the tree
+func resume() -> void: # Is used by parent/siblings in the tree
 	disable_buttons()
 	var tween = create_tween()
-	tween.tween_property(panel_container, "modulate", Color(1, 1, 1, 0), 0.2)
+	tween.tween_property(self, "modulate", Color(1.0, 1.0, 1.0, 0.0), 0.2)
 	await tween.finished
 	get_tree().paused = false
 	queue_free()
 
-func _on_resume_button_pressed():
+func _on_resume_button_pressed() -> void:
 	SoundManager.click.play()
 	resume()
 
-func _on_settings_button_pressed():
+func _on_settings_button_pressed() -> void:
 	SoundManager.click.play()
-	settings.visible = true
+	Global.game_controller.change_gui_scene("settings", false, true)
 
-func _on_exit_button_pressed():
+func _on_exit_button_pressed() -> void:
 	get_tree().paused = false
 	disable_buttons()
 	SoundManager.click.play()
 	SoundManager.disable_music()
-	get_tree().change_scene_to_file("res://gamedata/scene/main_menu/main_menu.tscn")
+	Global.game_controller.change_2d_scene("main_menu")
