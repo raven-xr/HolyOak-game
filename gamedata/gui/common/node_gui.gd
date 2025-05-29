0e0c9e5a168771af1927678d44a3adc62f82b638
Raven <raven-esq@yandex.ru>
2025-05-29 11:10:38 +0700

    Add the ability to close the NodeGUI with the "ui_cancel" action

--- gamedata/gui/common/node_gui.gd
+++ gamedata/gui/common/node_gui.gd
@@ -1,1 +1,15 @@
 extends Control
+
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
+func _input(_event: InputEvent) -> void:
+	if Input.is_action_just_pressed("ui_cancel"):
+		close()
+
+func close() -> void:
+	animation_player.play("Disappearance")
+	await animation_player.animation_finished
+	# Remove the value of the "current_gui_scene" variable if this NodeGUI is the current_gui_scene
+	if Global.game_controller.current_gui_scene == self:
+		Global.game_controller.current_gui_scene = null
+	queue_free()
