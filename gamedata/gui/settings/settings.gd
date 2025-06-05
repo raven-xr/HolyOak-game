c7c902f063b35bd1491b63c19b973052e52cd2f5
Raven <raven-esq@yandex.ru>
2025-06-05 10:49:53 +0700

    Settings: fix a bug: after you had closed Confirmation and had opened it again, settings weren't hidden this time, although it had to

--- gamedata/gui/settings/settings.gd
+++ gamedata/gui/settings/settings.gd
@@ -62,6 +62,7 @@ func _on_reset_settings_button_pressed() -> void:
 func _on_reset_progress_button_pressed() -> void:
 	SoundManager.click.play()
 	Global.game_controller.change_gui_scene("confirmation", false, true)
+	can_be_closed = false
 	var confirmation: NodeGUI = Global.game_controller.current_gui_scene
 	confirmation.connect("confirmed", Callable(self, "_on_confirmed"))
 	confirmation.connect("canceled", Callable(self, "_on_canceled"))
@@ -95,6 +96,7 @@ func _on_canceled() -> void:
 	visible = true
 	var tween = create_tween()
 	tween.tween_property(self, "modulate:a", 1.0, 0.15)
+	can_be_closed = true
 
 func _on_confirmed() -> void:
 	# Confirmation... confirmed?
