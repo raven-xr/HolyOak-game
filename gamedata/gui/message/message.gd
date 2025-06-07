0fed076d426b8a044036d1b4bd2afb1e9219dd61
Raven <raven-esq@yandex.ru>
2025-06-07 10:12:40 +0700

    Make the "message" scene the NodeGUI & improvements
    * Improve the animation
    * Improve the PanelContainer expanding mechanism
    * Add an autowrap mode

--- gamedata/gui/message/message.gd
+++ gamedata/gui/message/message.gd
@@ -1,19 +1,14 @@
-extends CanvasLayer
+extends NodeGUI
 
-@onready var panel_container = $PanelContainer
-@onready var label = $PanelContainer/Label
+@onready var panel_container: PanelContainer = $PanelContainer
+@onready var label: Label = $PanelContainer/Label
 
 func _ready() -> void:
-	# Scale
-	panel_container.scale = Vector2(UserSettings.gui_scale, UserSettings.gui_scale)
-	# Animate
-	panel_container.modulate = Color(1, 1, 1, 0)
-	var tween_1 = create_tween()
-	tween_1.tween_property(panel_container, "modulate", Color(1, 1, 1, 0.78125), 0.5)
-	await tween_1.finished
+	await get_tree().create_timer(1.5).timeout
 	var tween_2 = create_tween()
-	tween_2.tween_property(panel_container, "modulate", Color(1, 1, 1, 0), 1.5)
-	tween_2.connect("finished", queue_free)
+	tween_2.tween_property(panel_container, "modulate:a", 0.0, 1.0)
+	await tween_2.finished
+	queue_free()
 
 func set_text(text: String) -> void:
 	label.text = text
