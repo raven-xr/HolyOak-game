1cdaefe31c2bcebf6da79ea1d28abf1eaa1eebce
Raven <raven-esq@yandex.ru>
2025-07-24 09:19:52 +0700

    Remove the input-filter of the NodeGUI

--- gamedata/gui/common/node_gui.gd
+++ gamedata/gui/common/node_gui.gd
@@ -1,21 +1,11 @@
 extends Control
-## Don't overwrite the "_on_ready", "_input" and "close" functions if this isn't a necessary
 class_name NodeGUI
-
-## Uncheck the box if it isn't implied that this scene can be closed with the "ui_cancel" action (e.g. Esc)
-@export var can_be_closed: bool = true
 
 func _on_ready() -> void:
 	scale = Vector2(UserSettings.gui_scale, UserSettings.gui_scale)
 	modulate.a = 0.0
 	var tween = create_tween()
 	tween.tween_property(self, "modulate:a", 1.0, 0.15)
-
-func _input(_event: InputEvent) -> void:
-	# All GUI-scenes have to be NodeGUIs or at least have the "can_be_closed" property
-	if Input.is_action_just_pressed("ui_cancel") and can_be_closed:
-		SoundManager.click.play()
-		close()
 
 func close() -> void:
 	var tween = create_tween()
