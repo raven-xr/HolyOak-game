b1eac3a84decd081582a07d61656398809da0907
Raven <raven-esq@yandex.ru>
2025-08-07 11:48:06 +0700

    Vignette: remove the transparency shift and the radius shift
    * Remove the transparency shift and the radius shift, because they were not used and just caused some troubles
    * Increase the pulse amplitude from 0.2 to 0.3
    * Code improvements

--- gamedata/FX/vignette/vignette.gd
+++ gamedata/FX/vignette/vignette.gd
@@ -12,14 +12,12 @@ extends ColorRect
 ## The pulse amplitude determines the maximum and minimum radius of the vignette, 
 ## so that the maximum is the current radius + (amplitude / 2), 
 ## and the minimum is the radius + (amplitude / 2)
-@export var pulse_amplitude: float = 0.2
+@export var pulse_amplitude: float = 0.3
 ## How long will it take between the pulsation from the minimum to the maximum
 @export var pulse_time: float = 1.5
 
 @export_group("Shift")
 @export var max_vignette_color_shift: Color = Color(1.0, 1.0, 1.0, 1.0)
-@export var max_transparency_shift: float = 1.0
-@export var max_radius_shift: float = 1.0
 
 var vignette_color_shift: Color = Color(0.0, 0.0, 0.0, 0.0):
 	set(value):
@@ -28,14 +26,6 @@ var vignette_color_shift: Color = Color(0.0, 0.0, 0.0, 0.0):
 		value.b = min(value.b, max_vignette_color_shift.b)
 		value.a = min(value.a, max_vignette_color_shift.a)
 		vignette_color_shift = value
-var transparency_shift: float = 0.0:
-	set(value):
-		value = min(value, max_transparency_shift)
-		transparency_shift = value
-var radius_shift: float = 0.0:
-	set(value):
-		value = min(value, max_radius_shift)
-		radius_shift = value
 
 func _ready() -> void:
 	# Resets shader
@@ -54,6 +44,7 @@ func set_vignette_color(value: Color, duration: float) -> void:
 ## then returns it in {max(value.r, value.g, value.b, value.a) * 5} seconds
 func shift_vignette_color(value: Color, duration: float) -> void:
 	# Restart the shift timer if it's not paused
+	shift_timer.wait_time = max(shift_timer.wait_time, 5.0 + duration)
 	if not shift_timer.paused:
 		shift_timer.stop()
 	shift_timer.start()
@@ -65,9 +56,7 @@ func shift_vignette_color(value: Color, duration: float) -> void:
 		return
 	var tween = create_tween()
 	tween.tween_property(material, "shader_parameter/vignette_color", material.get("shader_parameter/vignette_color") + value, duration)
-	await tween.finished
 	vignette_color_shift += value
-	shift_timer.wait_time = 5.0
 
 ## Changes color within {duration} seconds[br]
 ## Set a small value of the duration if you want to change it instantly
@@ -76,44 +65,12 @@ func set_transparency(value: float, duration: float) -> void:
 	tween.tween_property(material, "shader_parameter/transparency", value, duration)
 	await tween.finished
 
-## Shifts the transparency by {value} within {duration} seconds and
-## then returns it in {value * 5} seconds
-func shift_transparency(value, duration: float) -> void:
-	# Restart the shift timer if it's not paused
-	if not shift_timer.paused:
-		shift_timer.stop()
-	shift_timer.start()
-	# Shift it if the property hasn't reached maximum yet
-	if transparency_shift == max_transparency_shift:
-		return
-	var tween = create_tween()
-	tween.tween_property(material, "shader_parameter/transparency", material.get("shader_parameter/transparency") + value, duration)
-	await tween.finished
-	transparency_shift += value
-	shift_timer.wait_time = 5.0
-
 ## Changes radius within {duration} seconds[br]
 ## Set a small value of the duration if you want to change it instantly
 func set_radius(value: float, duration: float = 0.001) -> void:
 	var tween = create_tween()
 	tween.tween_property(material, "shader_parameter/radius", value, duration)
 	await tween.finished
-
-## Shifts the radius by {value} within {duration} seconds and
-## then returns it in {value * 5} seconds
-func shift_radius(value, duration: float) -> void:
-	# Restart the shift timer if it's not paused
-	if not shift_timer.paused:
-		shift_timer.stop()
-	shift_timer.start()
-	# Shift it if the property has't reached maximum yet
-	if radius_shift == max_radius_shift:
-		return
-	var tween = create_tween()
-	tween.tween_property(material, "shader_parameter/radius", material.get("shader_parameter/radius") + value, duration)
-	await tween.finished
-	radius_shift += value
-	shift_timer.wait_time = 5.0
 
 func pulse_up(original_radius: float) -> void:
 	var inaccuracy: float = randf_range(0.0, pulse_amplitude / 4) # Makes the animation more dynamic
@@ -142,12 +99,4 @@ func _on_shift_timer_timeout() -> void:
 	if vignette_color_shift != Color(0.0, 0.0, 0.0, 0.0):
 		# Returns the color shift in {6.0 * max(vignette_color_shift.r, vignette_color_shift.g, vignette_color_shift.b, vignette_color_shift.a)} seconds.
 		# e.g. Color(1.0, 0.9, 0.0, 0.1) will be shifted in 6.0 seconds
-		tween.parallel().tween_property(material, "shader_parameter/vignette_color", material.get("shader_parameter/vignette_color") - vignette_color_shift, 6.0 * max(vignette_color_shift.r, vignette_color_shift.g, vignette_color_shift.b, vignette_color_shift.a))
-	if transparency_shift != 0.0:
-		# Returns the transparency shift in {6.0 * transparency_shift} seconds.
-		# e.g. 2.0 will be shifted in 15 seconds
-		tween.parallel().tween_property(material, "shader_parameter/transparency", material.get("shader_parameter/transparency") - transparency_shift, 6.0 * transparency_shift)
-	if radius_shift != 0.0:
-		# Returns the radius shift in {6.0 * radius_shift} seconds.
-		# e.g. 2.0 will be shifted in 6.0 seconds
-		tween.parallel().tween_property(material, "shader_parameter/radius", material.get("shader_parameter/radius") - radius_shift, 6.0 * radius_shift)
+		tween.tween_property(material, "shader_parameter/vignette_color", material.get("shader_parameter/vignette_color") - vignette_color_shift, 6.0 * max(vignette_color_shift.r, vignette_color_shift.g, vignette_color_shift.b, vignette_color_shift.a))
