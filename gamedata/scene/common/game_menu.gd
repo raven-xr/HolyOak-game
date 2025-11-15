b65045078f6fcb8ccc06cafe8b07939f7bb0cd4f
Raven <raven-esq@yandex.ru>
2025-11-15 19:05:06 +0700

    GameMenu: add the exit confirmation

--- gamedata/scene/common/game_menu.gd
+++ gamedata/scene/common/game_menu.gd
@@ -7,13 +7,13 @@ extends NodeGUI
 func _ready() -> void:
 	get_tree().paused = true
 
-func disable_buttons() -> void:
-	resume_button.disabled = true
-	settings_button.disabled = true
-	exit_button.disabled = true
+func set_buttons_disabled(disabled: bool = true) -> void:
+	resume_button.disabled = disabled
+	settings_button.disabled = disabled
+	exit_button.disabled = disabled
 
 func close() -> void:
-	disable_buttons()
+	set_buttons_disabled()
 	get_tree().paused = false
 	var tween = create_tween()
 	tween.tween_property(self, "modulate", Color(1.0, 1.0, 1.0, 0.0), 0.15)
@@ -31,7 +31,16 @@ func _on_settings_button_pressed() -> void:
 
 func _on_exit_button_pressed() -> void:
 	get_tree().paused = false
-	disable_buttons()
+	set_buttons_disabled()
 	SoundManager.click.play()
 	SoundManager.disable_music()
+	Global.game_controller.change_gui_scene("confirmation")
+	Global.game_controller.current_gui_scene.set_text("Вы уверены? Весь прогресс будет безвозвратно утерян!")
+	Global.game_controller.current_gui_scene.connect("confirmed", Callable(self, "_on_exit_confirmed"))
+	Global.game_controller.current_gui_scene.connect("canceled", Callable(self, "_on_exit_canceled"))
+
+func _on_exit_confirmed() -> void:
 	Global.game_controller.change_2d_scene("main_menu")
+
+func _on_exit_canceled() -> void:
+	set_buttons_disabled(false)
